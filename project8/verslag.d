7e193b68494de47fd760ed5c6900e96d9a48182e
Paul Sohier <paul@phpbb.com>
2012-06-20 09:30:54 +0200

    Hoop zooi die ik hier nog had...

---
+++ project8/verslag.d
@@ -0,0 +1,62 @@
+# vim: ft=make
+.PHONY: verslag._graphics
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/generic/babel/babel.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/generic/ifxetex/ifxetex.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/generic/oberdiek/atbegshi.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/generic/oberdiek/bigintcalc.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/generic/oberdiek/bitset.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/generic/oberdiek/etexcmds.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/generic/oberdiek/ifluatex.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/generic/oberdiek/ifpdf.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/generic/oberdiek/ifvtex.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/generic/oberdiek/infwarerr.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/generic/oberdiek/intcalc.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/generic/oberdiek/kvsetkeys.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/generic/oberdiek/ltxcmds.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/generic/oberdiek/pdftexcmds.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/latex/algorithms/algorithmic.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/latex/amsfonts/amsfonts.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/latex/base/ifthen.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/latex/base/inputenc.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/latex/base/latexsym.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/latex/base/report.cls
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/latex/eurosym/eurosym.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/latex/graphics/color.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/latex/graphics/graphics.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/latex/graphics/graphicx.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/latex/graphics/keyval.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/latex/graphics/trig.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/latex/hyperref/hyperref.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/latex/hyperref/nameref.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/latex/listings/listings.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/latex/listings/lstmisc.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/latex/ltxmisc/url.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/latex/oberdiek/hycolor.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/latex/oberdiek/kvoptions.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/latex/oberdiek/refcount.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/latex/oberdiek/xcolor-patch.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/latex/pslatex/pslatex.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/latex/rotating/rotating.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/latex/tools/array.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/latex/tools/calc.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: /usr/share/texmf-texlive/tex/latex/tools/tabularx.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: bronnen.tex
+verslag.aux verslag.aux.make verslag.d verslag.pdf: hoofdstuk_02.tex
+verslag.aux verslag.aux.make verslag.d verslag.pdf: hoofdstuk_03.tex
+verslag.aux verslag.aux.make verslag.d verslag.pdf: hoofdstuk_04.tex
+verslag.aux verslag.aux.make verslag.d verslag.pdf: hoofdverslag.tex
+verslag.aux verslag.aux.make verslag.d verslag.pdf: inleiding.tex
+verslag.aux verslag.aux.make verslag.d verslag.pdf: modelverslag.sty
+verslag.aux verslag.aux.make verslag.d verslag.pdf: pagnum.tex
+verslag.aux verslag.aux.make verslag.d verslag.pdf: samenvatting.tex
+verslag.aux verslag.aux.make verslag.d verslag.pdf: verslag.tex
+.SECONDEXPANSION:
+-include plaatjes/schema.png.gpi.d
+verslag.d: $$(call graphics-source,plaatjes/schema.png)
+verslag.pdf verslag._graphics: $$(call graphics-target,plaatjes/schema.png)
+-include plaatjes/pcb_ontwerp.png.gpi.d
+verslag.d: $$(call graphics-source,plaatjes/pcb_ontwerp.png)
+verslag.pdf verslag._graphics: $$(call graphics-target,plaatjes/pcb_ontwerp.png)
+-include plaatjes/scheme_rs485.png.gpi.d
+verslag.d: $$(call graphics-source,plaatjes/scheme_rs485.png)
+verslag.pdf verslag._graphics: $$(call graphics-target,plaatjes/scheme_rs485.png)
